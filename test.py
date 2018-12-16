a62c160cfc2aba77c53900da785074fa0211e9ad
Bel2015 <245358009@qq.com>
2018-12-16 20:37:37 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,27 @@
+import find_sum
+#只有一个输入
+#find_sum.find_sum(1)
+#输入参数过多
+#find_sum.find_sum(1,2,3)
+#输入的第一个参数不是列表"
+#find_sum.find_sum('abc',1)
+#输入的第二个参数不是整数
+#find_sum.find_sum([1,2],'a')
+#输入的list不是整形的list
+#find_sum.find_sum([1,2,1],5)
+#输入的list不是有序的list
+#find_sum.find_sum(['a',2,1],5)
+#输入的list是一个无重复元素并且有打印值的list
+#find_sum.find_sum([1,2,3],4)
+#输入的list有重复值，并且重复值等于sum的一半且在list的中间部分
+#find_sum.find_sum([1,2,3,4,4,4,4,5,5,5,5,6],8)
+#输入的list有重复值，并且重复值等于sum的一半且在list的尾端
+#find_sum.find_sum([1,2,3,5,5,5],10)
+#输入的list有重复值，并且重复值不等于sum的一半且在list的中间部分
+#find_sum.find_sum([1,2,3,5,5,5,10,20],12)
+#输入的list有重复值，并且重复值不等于sum的一半且在list的尾端
+#find_sum.find_sum([1,2,3,5,5,5],8)
+#输入的list有负数
+#find_sum.find_sum([-1,0,2,3,5,5,5],4)
+#输入的list和sum无返回结果
+#find_sum.find_sum([-1,0,2,3,5,5,5],1000)
