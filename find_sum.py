59668dca1cad8cc3434bb4dc0492d5317edfb7f6
Bel2015 <245358009@qq.com>
2018-12-16 21:36:02 +0800

    Update find_sum.py

--- find_sum.py
+++ find_sum.py
@@ -14,25 +14,11 @@ def find_sum(list_input,sum_num):
 		if (list_input[i] > list_input[i+1]):
 			print("error: Your input is not a sorted list!")	#判断是否是有序的列表
 			return
-	dict_list={}	
 	list_output=[]
 	list_temp=[]
 	for i in range(len(list_input)):
-		if(list_input[i] > sum_num/2):
-			break
-		else:
-			dict_list[sum_num-list_input[i]] = list_input[i]    #将列表中的数字放到字典中，key值是sum_num减去列表的值，value是列表的值
-	for i in range(len(list_input)-1,-1,-1):
-		if(dict_list.get(list_input[i])):
-			if(list_input[i] == sum_num/2): 
-				if(i == len(list_input)-1):
-					continue
-				elif(list_input[i+1] == sum_num/2):
-					list_temp = [dict_list.get(list_input[i]),list_input[i]]
-					list_output.append(list_temp)
-				else:
-					continue
-			else:
-				list_temp = [dict_list.get(list_input[i]),list_input[i]]
+		for j in range(i+1, len(list_input)):
+			if list_input[i] + list_input[j] == sum_num:
+				list_temp = [list_input[i],list_input[j]]
 				list_output.append(list_temp)
 	print(str(list_output)[1:-1])
